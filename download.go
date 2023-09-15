7d888fd5dcedcf975602cf9bd982e9c1fa325e29
nopdan <me@nopdan.com>
2023-09-15 09:09:05 +0800

    feat: save cache

--- download.go
+++ download.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/cavaliergopher/grab/v3"
@@ -13,27 +14,42 @@ import (
 // Download a file from a given URL and verifies its integrity using SHA256 checksum.
 func (info *Info) download() {
 	client := grab.NewClient()
-	req, _ := grab.NewRequest("", info.URL)
-	req.NoStore = true // don't store the downloaded file
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		fmt.Printf("failed to get cache dir\n")
+		panic(err)
+	}
+	dir = filepath.Join(dir, "zigo")
+	req, err := grab.NewRequest(dir, info.URL)
+	if err != nil {
+		fmt.Printf("failed to create request\n")
+		panic(err)
+	}
 
 	// Start download
 	resp := client.Do(req)
-	fmt.Printf("Downloading %v...\n", req.URL())
 
-	// Start UI loop
-	t := time.NewTicker(200 * time.Millisecond)
-	defer t.Stop()
-	bar := progressbar.DefaultBytes(resp.Size())
-Loop:
-	for {
-		select {
-		case <-t.C:
-			bar.Set64(resp.BytesComplete())
-		case <-resp.Done:
-			break Loop
+	// Check if the download did resume
+	if resp.DidResume {
+		resp.Wait()
+		fmt.Printf("Load cache from %s\n", filepath.Join(dir, info.FileName))
+	} else {
+		fmt.Printf("Downloading %v...\n", req.URL())
+		// Start UI loop
+		t := time.NewTicker(200 * time.Millisecond)
+		defer t.Stop()
+		bar := progressbar.DefaultBytes(resp.Size())
+	Loop:
+		for {
+			select {
+			case <-t.C:
+				bar.Set64(resp.BytesComplete())
+			case <-resp.Done:
+				break Loop
+			}
 		}
+		bar.Set64(resp.BytesComplete())
 	}
-	bar.Set64(resp.BytesComplete())
 
 	// Check errors
 	if err := resp.Err(); err != nil {
@@ -48,8 +64,14 @@ Loop:
 	info.data, _ = resp.Bytes()
 	h := sha256.New()
 	h.Write(info.data)
+
+	// Mismatch
 	if fmt.Sprintf("%x", h.Sum(nil)) != info.Shasum {
 		fmt.Printf("sha256 mismatch. want: %s, got: %x\n", info.Shasum, h.Sum(nil))
+		// Remove downloaded file
+		os.Remove(filepath.Join(dir, info.FileName))
 		os.Exit(1)
 	}
+
+	fmt.Printf("Save cache to %s\n", filepath.Join(dir, info.FileName))
 }
